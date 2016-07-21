67aded394441bcd59f6bf018d0c352e0bd527cfa
Kendrick <kendrick-k@users.noreply.github.com>
2016-07-21 12:57:14 +0200

    namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MediaFigaro\Analytics\DependencyInjection;
+namespace MediaFigaro\GoogleAnalyticsApi\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
